96637e119ac153a2d4a9b683b99a78d58383afd4
jogoshugh <jgough@JGough.corp.versionone.net>
2013-02-11 17:11:22 -0500

    test

--- dynamic/main.js
+++ dynamic/main.js
@@ -12,6 +12,7 @@ requirejs.config({
 
 require(['handlebars', 'boot', 'text!./hello.html'], function(hb, boot, helloTmpl) {
 	var context = {title: 'VersionOne', folks: ['Mark K', 'Mark I', 'Acey B']};
+	console.log(helloTmpl);
 	var template = Handlebars.compile(helloTmpl);
 	var output = template(context);
 	document.write(output);
