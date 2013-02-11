3d6952b054f1858c0506892c1e9d14dcffd24a7b
jogoshugh <jgough@JGough.corp.versionone.net>
2013-02-11 13:49:26 -0500

    Added for dynamic test

---
+++ dynamic/main.js
@@ -0,0 +1,12 @@
+reaquirejs.confg({
+	shim: {
+		'handlebars' : {
+			exports: 'Handlebars'
+		}
+	}
+});
+
+require('handlebars', 'boot', function(Handlebars, boot) {
+	document.write("Hello from boot");
+	console.log(Handlebars);
+});
