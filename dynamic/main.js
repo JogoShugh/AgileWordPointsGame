04ecf61cb1fbd3833bca93709657edc8e02c88bb
jogoshugh <jgough@JGough.corp.versionone.net>
2013-02-11 14:36:32 -0500

    Dynamic html loading from handlebars template

--- dynamic/main.js
+++ dynamic/main.js
@@ -12,13 +12,11 @@ requirejs.config({
 
 
 require(['handlebars', 'boot'], function(hb, boot) {
-	console.log(Handlebars);
-	console.log('Boot loaded.');
-});
-
-
-/*
-require(['boot'], function(boot) {
+	var context = {title: 'VersionOne', folks: ['Mark K', 'Mark I', 'Acey B']};
+	var source = "<p>Welcome to {{title}}, <ul>{{#each folks}}<li>{{this}}</li>{{//each}}</ul></p>";
+	var template = Handlebars.compile(source);
+	var output = template(context);
+	document.write(output);
 
 });
-*/
+
