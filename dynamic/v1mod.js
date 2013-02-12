61f7a225e8a3a6218974834efe95cc3425b57822
jogoshugh <jgough@JGough.corp.versionone.net>
2013-02-12 00:50:10 -0500

    Betters

--- dynamic/v1mod.js
+++ dynamic/v1mod.js
@@ -3,6 +3,6 @@ require.config({
 });
 
 require(['jquery', 'v1json'], function($, v1) {
-	console.log(v1);
-	$.ajax("http://eval.versionone.net/platformtest/rest-1.v1/Data/Story/1002?accept=application/json&sel=Name,Description,Estimate").done(function(data) { console.log(v1.jsonClean(data)); });
+	window.v1 = v1;
+	$.ajax("http://eval.versionone.net/platformtest/rest-1.v1/Data/Story/1002?accept=application/json&sel=Name,Description,Estimate").done(function(data) { window.cleanData = v1.jsonClean(data); });
 });
