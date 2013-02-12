e8cf6fd48d321c73a55d866ea579eba981431b0c
jogoshugh <jgough@JGough.corp.versionone.net>
2013-02-12 00:45:14 -0500

    jquery up in that!

--- dynamic/v1mod.js
+++ dynamic/v1mod.js
@@ -2,7 +2,7 @@ require.config({
 	baseUrl: "http://jogoshugh.github.com/AgileWordPointsGame/dynamic"
 });
 
-require(['v1json'], function(v1) {
+require(['jquery', 'v1json'], function($, v1) {
 	console.log(v1);
 	$.ajax("http://eval.versionone.net/platformtest/rest-1.v1/Data/Story/1002?accept=application/json&sel=Name,Description,Estimate").done(function(data) { console.log(v1.jsonClean(data)); });
 });
